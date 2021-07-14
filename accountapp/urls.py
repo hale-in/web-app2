458c373e43ae32e5781d6eb6e86deec972b1d779
hale-in <78592027+hale-in@users.noreply.github.com>
2021-07-14 14:59:27 +0900

    7/14 3ban class commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from accountapp.views import nd_project
+from accountapp.views import nd_project, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns =[
-    path('nd_project/', nd_project, name='nd_project')
+    path('nd_project/', nd_project, name='nd_project'),
+    path('create/', AccountCreateView.as_view, name='create')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+
 from accountapp.models import NewModel
 
 
@@ -17,3 +21,9 @@ def nd_project(request):
         data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
                       context={'data_list': data_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
