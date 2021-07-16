e0d282e6a3990f3bfecd9f8b4445f16d436bb865
hale-in <78592027+hale-in@users.noreply.github.com>
2021-07-16 10:53:49 +0900

    7/16 3ban class commit 4

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import nd_project, AccountCreateView
+from accountapp.views import nd_project, AccountCreateView, AccountDetailView
 
 app_name = 'accountapp'
 
@@ -9,5 +9,6 @@ urlpatterns =[
     path('nd_project/', nd_project, name='nd_project'),
     path('create/', AccountCreateView.as_view(), name='create'),
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
 ]
