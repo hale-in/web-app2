aa142581d721deafa35c0a1996546bd7bb0da6bc
hale-in <78592027+hale-in@users.noreply.github.com>
2021-08-27 09:27:42 +0900

    8/25 3ban class commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,12 +1,11 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import nd_project, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
+from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
 
 urlpatterns =[
-    path('nd_project/', nd_project, name='nd_project'),
     path('create/', AccountCreateView.as_view(), name='create'),
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
--- accountapp/views.py
+++ accountapp/views.py
@@ -14,24 +14,10 @@ from accountapp.models import NewModel
 from articleapp.models import Article
 
 
-@login_required
-def nd_project(request):
-    if request.method == "POST":
-        temp = request.POST.get("input_text")
-        model_instance = NewModel()
-        model_instance.text = temp
-        model_instance.save()
-
-        return HttpResponseRedirect(reverse('accountapp:nd_project'))
-    else:
-        data_list = NewModel.objects.all()
-        return render(request, 'accountapp/hello_world.html',
-                      context={'data_list': data_list})
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:nd_project')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/create.html'
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
@@ -63,6 +49,6 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:nd_project')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/delete.html'
 
